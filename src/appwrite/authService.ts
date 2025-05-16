78486b21f91605221c31f40b5d684116738f2af4
Dev Sharma <devsharma1683@gmail.com>
2025-05-16 23:34:00 +0530

    make changes in create account function for storing role in database

--- src/appwrite/authService.ts
+++ src/appwrite/authService.ts
@@ -1,10 +1,38 @@
-import { create } from "domain";
 import { appwrite } from "./appwrite";
 
 export const authService = {
-    async createAccount(email: string, password: string, name: string){
+    
+    async createAccount(email: string, password: string, name: string,  role: string){
         try {
-          return appwrite.account.create(appwrite.ID.unique(), email, password, name)
+          const user = await appwrite.account.create(appwrite.ID.unique(), email, password, name);
+
+          if(user){
+               const logInUser = await appwrite.account.createEmailPasswordSession(email,password);
+
+               if (logInUser) {
+                    const currentUser = await appwrite.account.get();
+                
+                    const databaseID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
+                    const userRoleCollectionID = process.env.NEXT_PUBLIC_APPWRITE_USER_ROLE_COLLECTION_ID;
+          
+                    console.log(databaseID);
+                    console.log(userRoleCollectionID);  
+
+                    await appwrite.databases.createDocument(
+                      databaseID!,
+                      userRoleCollectionID!,
+                      appwrite.ID.unique(),
+                      {
+                        userId: currentUser.$id,
+                        role: role,  
+                      }
+                    );
+                    return true;
+                }
+            } else{
+                return false;
+            }
+
         } catch (error : any) {
             console.log("Error in creating an account :" ,error);
         }
