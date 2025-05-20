9d1b09b1ffe351e5b7da3af9c59437ea56ef9378
Dev Sharma <devsharma1683@gmail.com>
2025-05-20 13:32:08 +0530

    added new function for returning the user data and userRole for a component

--- src/appwrite/authService.ts
+++ src/appwrite/authService.ts
@@ -76,6 +76,28 @@ export const authService = {
         }
     },   
 
+    async loadCurrentuser(){
+        try {
+           const user = await appwrite.account.get();
+
+           const databaseID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
+           const userRoleCollectionID = process.env.NEXT_PUBLIC_APPWRITE_USER_ROLE_COLLECTION_ID  
+
+           const response = await appwrite.databases.listDocuments(
+            databaseID!,
+            userRoleCollectionID!,
+            [Query.equal("userId", user.$id)]
+           )
+           
+           const userRole = response.documents[0]?.role || "guest";
+
+           return {user, userRole};
+
+        } catch (error: any) {
+            console.log("Error in loading the user :", error);
+        }
+    },
+
     async logout(){
         return appwrite.account.deleteSession('current')
     }
