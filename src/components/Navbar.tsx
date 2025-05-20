96e03b732a7d5a1128cf6bb4e78be2612ef8de15
Dev Sharma <devsharma1683@gmail.com>
2025-05-20 12:14:26 +0530

    render navbar based user role

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import styles from "./Navbar.module.css"
 import { useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
 import { authService } from "@/appwrite/authService";
 import { clearUsers } from "@/redux/authSlice";
 import { useDispatch } from "react-redux";
@@ -11,12 +10,50 @@ import { useRouter } from "next/navigation";
 
 export default function Navbar(){
 
+    const user = useSelector((state:any)=> state.auth.user);
+    const userRole = user?.role;
+
+    const dispatch = useDispatch();
+    const router = useRouter();
+
+    const handleLogout = async()=>{
+        const logout = await authService.logout()
+        if (logout) {
+            dispatch(clearUsers());
+            router.push("/login")
+        }
+    }
 
     return(
         <nav className="my-5 flex justify-center items-center gap-2">
-            <Link href="/" className={styles.tabs}>Home</Link>
-            <Link href="/login" className={styles.tabs}>Login</Link>
-            <Link href="/signUp"  className={styles.tabs}>SignUp</Link>
+            {/* 🧑‍🌾 Farmer Navbar */}
+            {userRole === "farmer" && (
+               <>
+                 <Link href={`/dashboard-${userRole}`} className={styles.tabs}>Dashboard</Link>
+                 <Link href="/add-item" className={styles.tabs}>Add Item</Link>
+                 <Link href="/orders" className={styles.tabs}>Orders</Link>
+                 <button onClick={handleLogout} className={styles.tabs}>Logout</button>
+               </>   
+            )}
+            
+            {/* 🧑‍💼 Consumer Navbar */}
+            {userRole === "consumer" && (
+                <>
+                 <Link href={`/dashboard-${userRole}`} className={styles.tabs}>Home</Link>
+                  <Link href="/search" className={styles.tabs}>Search</Link>
+                  <Link href="/profile" className={styles.tabs}>Profile</Link>
+                  <button onClick={handleLogout} className={styles.tabs}>Logout</button>
+                </>
+            )}
+            
+            {/* 🌐 Public Navbar (Not Logged In) */}
+            {!userRole && (
+                <>
+                  <Link href="/" className={styles.tabs}>Home</Link>
+                  <Link href="/login" className={styles.tabs}>Login</Link>
+                  <Link href="/signUp" className={styles.tabs}>Sign Up</Link>
+                </>
+            )}
         </nav>
     )
 }
