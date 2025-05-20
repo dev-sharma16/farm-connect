de4fbead45c8e0d160b6b3acc4e96248445ac4d3
Dev Sharma <devsharma1683@gmail.com>
2025-05-20 13:31:29 +0530

    component for loading the userRole

---
+++ src/components/authLoader/AuthLoader.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { authService } from "@/appwrite/authService";
+import { setUsers } from "@/redux/authSlice";
+
+export default function AuthLoader(){
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    async function fetchUser(){
+      const userData = await authService.loadCurrentuser();
+      
+      if (userData) {
+        const {user, userRole} = userData;
+        dispatch(setUsers({...user, role: userRole }))
+      }
+    }
+
+    fetchUser();
+  },[])
+
+  return null;
+}
