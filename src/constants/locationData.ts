2f65a4ee653dce3259e3f8dc23dd0e5edc8f4289
Dev Sharma <devsharma1683@gmail.com>
2025-05-22 22:07:55 +0530

    location data for dropdown for add item form

---
+++ src/constants/locationData.ts
@@ -0,0 +1,26 @@
+export const STATES_AND_CITIES = [
+  {
+    state: 'Uttar Pradesh',
+    cities: ['Lucknow', 'Kanpur', 'Noida', 'Varanasi', 'Agra'],
+  },
+  {
+    state: 'Maharashtra',
+    cities: ['Mumbai', 'Pune', 'Nagpur', 'Nashik', 'Aurangabad'],
+  },
+  {
+    state: 'Bihar',
+    cities: ['Patna', 'Gaya', 'Muzaffarpur', 'Bhagalpur', 'Darbhanga'],
+  },
+  {
+    state: 'Punjab',
+    cities: ['Amritsar', 'Ludhiana', 'Jalandhar', 'Patiala', 'Bathinda'],
+  },
+  {
+    state: 'Madhya Pradesh',
+    cities: ['Bhopal', 'Indore', 'Jabalpur', 'Gwalior', 'Ujjain'],
+  },
+  {
+    state: 'Rajasthan',
+    cities: ['Jaipur', 'Udaipur', 'Kota', 'Jodhpur', 'Ajmer'],
+  },
+];
