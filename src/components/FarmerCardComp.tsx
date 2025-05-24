551fc16363ca6f17122b5eaadc5ba9b3ff44dd20
Dev Sharma <devsharma1683@gmail.com>
2025-05-24 10:22:29 +0530

    card comp for farmer dashboard

---
+++ src/components/FarmerCardComp.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+
+type FarmerCardProps = {
+  image: string;         
+  name: string;          
+  price: number | string;
+  availability: number | string;
+  onEdit: () => void;
+  onDelete: () => void;
+};
+
+export default function FarmerCard({
+  image,
+  name,
+  price,
+  availability,
+  onEdit,
+  onDelete,
+}: FarmerCardProps) {
+  return (
+        <div className="bg-white rounded-2xl shadow-md w-[300px] p-4 flex flex-col items-center gap-4">
+          <div className="w-full h-[180px] bg-gray-300 rounded-xl overflow-hidden flex justify-center items-center">
+            {image ? (
+                <Image
+                   src={image}
+                   alt={name}
+                   width={300}
+                   height={180}
+                   className="object-cover w-full h-full"
+                />
+            ) : (
+              <span className="text-gray-700">Placeholder for crop image</span>
+            )}
+          </div>
+    
+          <h2 className="text-xl font-semibold">{name}</h2>
+    
+          <div className="flex justify-between w-full px-4 text-base">
+            <p>
+              <span className="font-semibold">Price:</span> ₹{price}
+            </p>
+            <p>
+              <span className="font-semibold">Quanty:</span> {availability}
+            </p>
+          </div>
+    
+          <div className="flex gap-4 mt-2">
+            <button
+              onClick={onEdit}
+              className="bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-lg"
+            >
+              Edit Post
+            </button>
+            <button
+              onClick={onDelete}
+              className="bg-red-600 hover:bg-red-700 text-white font-medium py-2 px-4 rounded-lg"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+    );
+}
