5d5d3ab340c33c340df67e02f013141443842028
Dev Sharma <devsharma1683@gmail.com>
2025-05-31 18:01:41 +0530

    now the request are loading

--- src/app/request-consumer/page.tsx
+++ src/app/request-consumer/page.tsx
@@ -1,17 +1,68 @@
 "use client";
 
 import ConsumerReqCard from "@/components/ConsumerReqCard";
+import { crudService } from "@/appwrite/crudService";
+import { useEffect, useState } from "react";
+import { appwrite } from "@/appwrite/appwrite";
+
 
 export default function requestConsumer(){
+
+    const [requests, setRequests] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [userId, setUserId] = useState(""); 
+
+    useEffect(()=>{
+        const fetchRequest = async ()=>{
+            try {
+                setLoading(true);
+
+                const crrntUser = await appwrite.account.get();
+                const crrntUserId = crrntUser.$id;
+                setUserId(crrntUser.$id);
+
+                const requestData = await crudService.getRequestsByUser(crrntUserId);
+                setRequests(requestData?.documents || []);
+
+            } catch (error: any) {
+                console.log("Error in fetching Reqeusts : ", error);
+            } finally{
+                setLoading(false);
+            }
+        }
+
+        fetchRequest();
+    },[])    
+    
+    if (loading) {
+        return (
+            <div className="h-screen w-full flex items-center justify-center bg-[#f8fff2]">
+                <div className="text-lg">Loading posts...</div>
+            </div>
+        );
+    }
+
+    if (!requests || requests.length === 0) {
+        return (
+            <div className="min-h-[calc(100vh-64px)] w-full bg-[#b0dcb9] flex items-center justify-center py-20">
+                <div className="text-lg text-gray-700">No requests found</div>
+            </div>
+        );
+    }
+
     return(
         <div className="min-h-[calc(100vh-64px)] w-full bg-[#b0dcb9] flex items-start py-20">
-            <ConsumerReqCard
-              cropName="onion"
-              imageUrl="asdasd"
-              quantity={50}
-              farmerName="farmerXYZ"
-              status="pending"
-            ></ConsumerReqCard>
+            {requests.map((request, index)=>(
+                <ConsumerReqCard
+                  key = {request.$id || index}
+                  cropName={request.cropName}
+                  imageUrl={request.imageUrl}
+                  quantity={request.quantity}
+                  farmerName={request.farmerName}
+                  status={request.status}
+                ></ConsumerReqCard>
+
+            ))}
         </div>
     )
 } 
